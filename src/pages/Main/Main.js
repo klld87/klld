37c7ba9d439d31b4715a1e8848d21b415219451a
klld87 <klld87@protonmail.com>
2020-11-01 02:39:14 +0300

    Merge and add web3 a little bit

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 // styles
 import { Wrapper } from './styles';
@@ -13,6 +14,20 @@ import News from '../../components/News';
 import Footer from '../../components/Footer';
 
 const MainPage = () => {
+  const [isEthEnabled, setEthEnabled] = useState(false);
+
+  useEffect(() => {
+    const enableEth = async () => {
+      if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        await window.ethereum.enable();
+        setEthEnabled(true);
+      }
+    };
+
+    enableEth();
+  }, []);
+
   return (
     <Wrapper>
       <Header />
