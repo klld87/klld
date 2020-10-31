8a1af93e5869a600798563bf5092319be37dea5a
klld87 <klld87@protonmail.com>
2020-10-31 13:48:49 +0300

    feat: add new files

---
+++ components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {
+  Wrapper
+} from './styles'
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <p>
+        Header
+      </p>
+    </Wrapper>
+  )
+}
+
+export default Header
