d06ae9c93b34f705f3d7b510e52772e6cee5a100
klld87 <klld87@protonmail.com>
2020-10-31 14:05:04 +0300

    feat: add new files

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Flex, Box } from 'reflexbox'
+
+// styles
+import {
+  Wrapper
+} from './styles'
 
 // components
 import Header from '../components/Header'
 
 const IndexPage = () => {
   return (
-    <>
+    <Wrapper>
       <Header />
       <p>
         IndexPage
       </p>
-    </>
+    </Wrapper>
   )
 }
 
