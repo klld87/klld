8bb6b539439f6c77d2958c3582d2d7925fc3f75c
klld87 <klld87@protonmail.com>
2020-10-31 14:22:45 +0300

    font and typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    @font-face {
+    font-family: 'Kool Beans';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Kool Beans'), url('KoolBean.woff') format('woff');
+    }
+  }
+
   html,
   body {
+    height: 100%;
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    font-family: -apple-system, Roboto, BlinkMacSystemFont, Segoe UI, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+    background-repeat: no-repeat;
+    background-size: 100%;
+    background-image: linear-gradient(
+      188.62deg,
+      rgba(223, 233, 115, 0.55),
+      rgba(122, 61, 250, 0.385),
+      rgba(250, 61, 73, 0.385),
+      rgba(61, 250, 69, 0.385),
+      rgba(223, 233, 115, 0.55),
+      rgba(122, 61, 250, 0.385),
+      rgba(250, 61, 73, 0.385),
+      rgba(61, 250, 69, 0.385)
+    );
   }
 
   a {
