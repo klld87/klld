256e515a1274b56dc7b88f2f52171eb70a2d369b
klld87 <klld87@protonmail.com>
2020-10-31 15:07:51 +0300

    some

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
-import IndexPage from './index/index'
+import * as React from 'react'
+import { Flex, Box } from 'reflexbox'
+
+
+const IndexPage = () => {
+  return (
+
+
+      <Flex>
+        <Box>
+          Flex
+        </Box>
+      </Flex>
+
+  )
+}
 
 export default IndexPage
--- styles/typography.js
+++ styles/typography.js
@@ -1,15 +1,18 @@
 import styled from 'styled-components';
 
 export const SectionHeading = styled.h2`
+  margin: 0 0 0.2em;
   text-align: center;
   font-family: 'Kool Beans';
   font-size: 60px;
   font-style: normal;
   font-weight: 400;
-  line-height: 69px;
+  line-height: 1.3;
   color: #FF8484;
   -webkit-text-stroke: 4px #000; 
   text-stroke: 4px #000;
+
+  text-shadow: 4px 4px 15px rgba(0, 0, 0, 0.5);
 
   @media(min-width: 1024px) {
     font-size: 75px;
