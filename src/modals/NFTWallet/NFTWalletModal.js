575f1a5312f8134b988d1efaafb66f4cb87ea544
klld87 <klld87@protonmail.com>
2020-12-03 00:01:47 +0300

    refactor: NFTWalletModal

--- src/modals/NFTWallet/NFTWalletModal.js
+++ src/modals/NFTWallet/NFTWalletModal.js
@@ -4,7 +4,19 @@ import 'react-responsive-modal/styles.css';
 import { Flex, Box } from 'reflexbox';
 
 // Styles
-import { Wrapper, Title, CloseIcon, Body } from './styles';
+import {
+  Wrapper,
+  Title,
+  CloseIcon,
+  Body,
+  BalanceBlock,
+  BalanceValue,
+  BalanceType,
+  HarvestBlock,
+  Button,
+  ButtonTitle,
+  Footer,
+} from './styles';
 
 const NFTWalletModal = (props) => {
   const { open, onCloseModal, openNFTBar } = props;
@@ -13,15 +25,56 @@ const NFTWalletModal = (props) => {
     <Modal open={open} onClose={onCloseModal} center showCloseIcon={false}>
       <Wrapper>
         <Title>My bar</Title>
-        <CloseIcon />
+        <CloseIcon onClick={onCloseModal} />
         <Body>
-          <Flex>
-            <Box>
-              <div />
+          <Flex flexWrap="wrap" width="100%">
+            <Box width={[1, 1, 1 / 2, 1 / 2]}>
+              <BalanceBlock>
+                <BalanceValue>0.0000</BalanceValue>
+                <BalanceType>$AID balance</BalanceType>
+              </BalanceBlock>
+            </Box>
+            <Box width={[1, 1, 1 / 2, 1 / 2]}>
+              <BalanceBlock>
+                <BalanceValue>0.0000</BalanceValue>
+                <BalanceType>$KOOL balance</BalanceType>
+              </BalanceBlock>
             </Box>
           </Flex>
+          <HarvestBlock>
+            <BalanceValue>0.0000</BalanceValue>
+            <BalanceType>$AID available for harvest </BalanceType>
+          </HarvestBlock>
+          <Flex flexWrap="wrap" width="100%">
+            <Box width={[1, 1, 1 / 2, 1 / 2]} pr={[0, 0, '35px', '35px']}>
+              <Button>
+                <ButtonTitle>Harvest AID</ButtonTitle>
+              </Button>
+              <Button>
+                <ButtonTitle>Visit OPENSEA</ButtonTitle>
+              </Button>
+              <Button>
+                <ButtonTitle>Visit AID farm</ButtonTitle>
+              </Button>
+            </Box>
+            <Box width={[1, 1, 1 / 2, 1 / 2]} pl={[0, 0, '35px', '35px']}>
+              <Button>
+                <ButtonTitle>View on Etherscan</ButtonTitle>
+              </Button>
+              <Button onClick={openNFTBar}>
+                <ButtonTitle>View my NFTz</ButtonTitle>
+              </Button>
+              <Button>
+                <ButtonTitle>Sign out</ButtonTitle>
+              </Button>
+            </Box>
+          </Flex>
+          <Footer>
+            <Button onClick={onCloseModal} isCancel>
+              <ButtonTitle>Cancel</ButtonTitle>
+            </Button>
+          </Footer>
         </Body>
-        {/* <p onClick={openNFTBar}>View my NFTz</p> */}
       </Wrapper>
     </Modal>
   );
--- src/modals/NFTWallet/styles.js
+++ src/modals/NFTWallet/styles.js
@@ -8,14 +8,82 @@ export const Wrapper = styled.div`
   background-repeat: repeat;
   border-radius: 35px;
   padding: 27px 15px;
+  position: relative;
 
   @media (min-width: 1024px) {
-    padding: 70px 140px;
+    padding: 45px 100px 60px 100px;
   }
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  margin: 0;
+  font-weight: bold;
+  font-size: 55px;
+  line-height: 77px;
+  color: #263f26;
+  text-align: center;
+`;
 
-export const CloseIcon = styled.div``;
+export const CloseIcon = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  position: absolute;
+  top: 65px;
+  right: 80px;
+`;
 
 export const Body = styled.div``;
+
+export const BalanceBlock = styled.div``;
+
+export const BalanceValue = styled.h3`
+  font-family: 'Kool Beans';
+  font-size: 100px;
+  line-height: 115px;
+  text-align: center;
+  color: #263f26;
+  margin: 0 0 -30px 0;
+`;
+
+export const BalanceType = styled.p`
+  margin: 0;
+  font-family: 'Kool Beans';
+  font-size: 50px;
+  line-height: 57px;
+  text-align: center;
+  color: #263f26;
+`;
+
+export const HarvestBlock = styled.div`
+  width: 566px;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  border: 4px solid #263f26;
+  border-radius: 15px;
+  margin: 14px auto 40px auto;
+`;
+
+export const Button = styled.div`
+  height: 86px;
+  background: #263f26;
+  border: 4px solid #000000;
+  border-radius: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 0 14px 0;
+`;
+
+export const ButtonTitle = styled.p`
+  margin: 0;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 56px;
+  color: #fbf7e2;
+`;
+
+export const Footer = styled.div`
+  margin: 60px 0 0 0;
+`;
