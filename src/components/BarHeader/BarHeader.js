b7c3b653aab061c72f78aef86fa360d05895827f
klld87 <klld87@protonmail.com>
2020-12-05 12:51:01 +0300

    refactor: BarHeader component

--- src/components/BarHeader/BarHeader.js
+++ src/components/BarHeader/BarHeader.js
@@ -3,6 +3,9 @@ import * as React from 'react';
 // Components
 import Container from '../Container';
 
+// Hooks
+import useWindowSize from '../../hooks/useWindowSize';
+
 // Styles
 import {
   Wrapper,
@@ -10,31 +13,86 @@ import {
   LogoRow,
   KoolName,
   Logo,
+  Collapsed,
   Nav,
   NavLink,
+  NavLinkRouter,
   Button,
   ButtonTitle,
+  Burger,
+  MobileBalances,
+  BalanceItem,
+  Balances,
 } from './styles';
 
 const BarHeader = (props) => {
-  const { onOpenNFTWalletModal } = props;
+  const {
+    onOpenNFTWalletModal,
+    unlockWallet,
+    isWalletUnlocked,
+    koolBalance,
+    aidBalance,
+  } = props;
+
+  const [isCollapsed, setIsCollapsed] = React.useState(true);
+  const [windowWidth] = useWindowSize();
+
+  const toggleNavCollapsed = () => {
+    setIsCollapsed((prevCollapsed) => !prevCollapsed);
+  };
+
+  React.useEffect(() => {
+    setIsCollapsed(true);
+  }, [windowWidth]);
 
   return (
-    <Wrapper>
+    <Wrapper isCollapsed={isCollapsed}>
       <Container>
         <Row>
           <LogoRow>
             <Logo />
             <KoolName textStroke={2}>KOOL</KoolName>
           </LogoRow>
-          <Nav>
-            <NavLink>Main page</NavLink>
-            <NavLink>Special</NavLink>
-            <NavLink>KOOL Mixing</NavLink>
-          </Nav>
-          <Button onClick={onOpenNFTWalletModal}>
-            <ButtonTitle>My wallet</ButtonTitle>
-          </Button>
+          <Burger isCollapsed={isCollapsed} onClick={toggleNavCollapsed}>
+            <span />
+            <span />
+            <span />
+            <span />
+          </Burger>
+          <Collapsed isCollapsed={isCollapsed}>
+            <Nav>
+              <NavLinkRouter to="/">Main page</NavLinkRouter>
+              <NavLink>Special</NavLink>
+              <NavLink>KOOL Mixing</NavLink>
+              {isWalletUnlocked ? (
+                <Balances>
+                  <BalanceItem>
+                    {Number(koolBalance).toFixed(2)} $KOOL
+                  </BalanceItem>
+                  <BalanceItem>
+                    {Number(aidBalance).toFixed(2)} $AID
+                  </BalanceItem>
+                </Balances>
+              ) : null}
+            </Nav>
+            {isWalletUnlocked ? (
+              <Button onClick={onOpenNFTWalletModal}>
+                <ButtonTitle>My bar</ButtonTitle>
+              </Button>
+            ) : (
+              <Button onClick={unlockWallet}>
+                <ButtonTitle>unlock wallet</ButtonTitle>
+              </Button>
+            )}
+            {isWalletUnlocked ? (
+              <MobileBalances>
+                <BalanceItem>
+                  {Number(koolBalance).toFixed(2)} $KOOL
+                </BalanceItem>
+                <BalanceItem>{Number(aidBalance).toFixed(2)} $AID</BalanceItem>
+              </MobileBalances>
+            ) : null}
+          </Collapsed>
         </Row>
       </Container>
     </Wrapper>
--- src/components/BarHeader/styles.js
+++ src/components/BarHeader/styles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { NavLink as RouterLink } from 'react-router-dom';
 
 import logo from '../../images/logo.svg';
 import { TextStroke } from '../../styles/typography';
@@ -6,11 +7,22 @@ import { TextStroke } from '../../styles/typography';
 export const Wrapper = styled.div`
   width: 100%;
   height: 106px;
+  position: sticky;
+  top: 0;
+  z-index: 4;
   background: linear-gradient(
     180deg,
     #4f5a98 26.04%,
     rgba(79, 90, 152, 0) 100%
   );
+
+  @media (max-width: 1023px) {
+    ${({ isCollapsed }) =>
+      !isCollapsed &&
+      css`
+        background: #4f5a98;
+      `}
+  }
 `;
 
 export const Row = styled.div`
@@ -27,6 +39,8 @@ export const Logo = styled.div`
 `;
 
 export const LogoRow = styled.div`
+  flex: 1 0 auto;
+  margin-right: auto;
   flex-direction: row;
   display: flex;
   align-items: center;
@@ -44,19 +58,43 @@ export const KoolName = styled.p`
 
 export const Nav = styled.div`
   display: flex;
-  flex-direction: row;
   flex: 1;
-  margin: 0 0 0 50px;
+  flex-direction: column;
+  margin: 0;
+
+  @media (min-width: 1024px) {
+    margin: 0 0 0 50px;
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 export const NavLink = styled.a`
-  margin: 0 25px;
   font-weight: bold;
   font-size: 25px;
   line-height: 35px;
   text-align: center;
   color: #ffffff;
   text-shadow: -2px 0 #000000, 0 2px #000000, 2px 0 #000000, 0 -2px #000000;
+  margin: 0 0 15px 0;
+
+  @media (min-width: 1024px) {
+    margin: 0 25px;
+  }
+`;
+
+export const NavLinkRouter = styled(RouterLink)`
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 35px;
+  text-align: center;
+  color: #ffffff;
+  text-shadow: -2px 0 #000000, 0 2px #000000, 2px 0 #000000, 0 -2px #000000;
+  margin: 0 0 15px 0;
+
+  @media (min-width: 1024px) {
+    margin: 0 25px;
+  }
 `;
 
 export const Button = styled.div`
@@ -68,6 +106,8 @@ export const Button = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin: 0 auto;
+  margin-top: 10px;
 
   &:hover {
     cursor: pointer;
@@ -80,4 +120,111 @@ export const ButtonTitle = styled.p`
   font-size: 25px;
   line-height: 35px;
   color: #fbf7e2;
+  text-shadow: -2px 0 #000000, 0 2px #000000, 2px 0 #000000, 0 -2px #000000;
 `;
+
+export const Collapsed = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  @media (max-width: 1023px) {
+    display: ${({ isCollapsed }) => (isCollapsed ? 'none' : 'block')};
+    padding: 0 15px 30px;
+    text-align: center;
+
+    position: absolute;
+    width: 100vw;
+    top: 100%;
+    left: 50%;
+    right: 50%;
+    margin-left: -50vw;
+    margin-right: -50vw;
+    background: #4f5a98;
+    z-index: 5;
+  }
+`;
+
+export const Burger = styled.button.attrs(() => ({
+  type: 'button',
+}))`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 41px;
+  height: 41px;
+  outline: none;
+
+  span {
+    width: 41px;
+    height: 5px;
+    margin-bottom: 2px;
+    background: #ffffff;
+    transition: transform 0.2s;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
+
+  ${({ isCollapsed }) =>
+    !isCollapsed &&
+    css`
+      span:nth-child(2),
+      span:nth-child(3) {
+        transform: scale(0);
+      }
+
+      span:first-child {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        margin: auto;
+        transform: rotate(45deg);
+      }
+
+      span:last-child {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        margin: auto;
+        transform: rotate(-45deg);
+      }
+    `}
+`;
+
+export const MobileBalances = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 20px 0 0 0;
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const BalanceItem = styled.p`
+  margin: 0;
+  flex: 1;
+  text-align: center;
+  text-shadow: -2px 0 #000000, 0 2px #000000, 2px 0 #000000, 0 -2px #000000;
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 35px;
+  color: #ffffff;
+`;
+
+export const Balances = styled.div``;
